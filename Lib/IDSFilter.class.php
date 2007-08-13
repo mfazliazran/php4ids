4d63143aec32f9fb00682843d99343939ca4ebf5
Stefan.Gehrig.HN <Stefan.Gehrig.HN@aca2aae2-fa34-0410-83b6-69b06b8e1d93>
2007-08-13 10:00:05 +0000

    Update to resemble PHPIDS 0.3.1 functionality

--- Lib/IDSFilter.class.php
+++ Lib/IDSFilter.class.php
@@ -1,16 +1,75 @@
 <?php
+/**
+ * PHP4IDS
+ *
+ * Requirements: PHP4, DOM XML
+ *
+ * Copyright (c) 2007 Stefan Gehrig (gehrig@ishd.de)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @package PHP4IDS
+ * @subpackage Filter
+ */
+
+/**
+ * Abstract filter class
+ *
+ * A basic implementation of a filter object
+ *
+ * @author Stefan Gehrig (gehrig@ishd.de)
+ * @package PHP4IDS
+ * @subpackage Filter
+ * @abstract
+ * @version	0.1
+ */
 class IDSFilter
 {
+	/**
+	 * Filter rule
+	 *
+	 * @access 	private
+	 * @var 	mixed
+	 */
 	var $mRule;
+	/**
+	 * List of tags of the filter
+	 *
+	 * @access 	private
+	 * @var 	array
+	 */
 	var $mTags=array();
+	/**
+	 * Filter impact level
+	 *
+	 * @access 	private
+	 * @var 	int
+	 */
 	var $mImpact=0;
+	/**
+	 * Filter description
+	 *
+	 * @access 	private
+	 * @var 	string
+	 */
 	var $mDescription;
 
 	/**
+	 * Constructor
+	 *
+	 * @access 	public
 	 * @param	mixed $rule				Filter rule
 	 * @param	string $description		Filter description
 	 * @param	array $tags				List of tags
-	 * @param	int $impact			Filter impact level
+	 * @param	integer $impact			Filter impact level
+	 * @return 	void
 	 */
 	function IDSFilter($rule, $description, $tags, $impact)
 	{
@@ -21,6 +80,13 @@ class IDSFilter
 	}
 
 	/**
+	 * Abstract match method
+	 *
+	 * The concrete match process which returns a boolean to inform
+	 * about a match
+	 *
+	 * @abstract
+	 * @access 	public
 	 * @return	bool
 	 */
 	function Match($string)
@@ -28,18 +94,89 @@ class IDSFilter
 		error("This method needs to be overridden in subclasses");
 	}
 
+	/**
+	 * Get filter description
+	 *
+	 * @access 	public
+	 * @return	string	Filter description
+	 */
 	function Description() { return $this->mDescription; }
+	/**
+	 * Return list of tags
+	 *
+	 * @access 	public
+	 * @return	array	List of tags
+	 */
 	function Tags() { return $this->mTags; }
+	/**
+	 * Return filter rule
+	 *
+	 * @access 	public
+	 * @return	mixed	Filter rule
+	 */
 	function Rule() { return $this->mRule; }
+	/**
+	 * Get filter impact level
+	 *
+	 * @access 	public
+	 * @return	integer	Impact level
+	 */
 	function Impact() { return $this->mImpact; }
 }
 
+/**
+ * Regex filter class
+ *
+ * The filter class based on regular expression matching is the default
+ * filter class used in PHP4IDS.
+ *
+ * @author Stefan Gehrig (gehrig@ishd.de)
+ * @package PDP4IDS
+ * @subpackage Filter
+ * @version	0.2
+ */
 class IDSRegexpFilter extends IDSFilter
 {
+	/**
+	 * Returns PCRE flags (default 'ims')
+	 *
+	 * @access 	public
+	 * @package PHP4IDS
+	 * @return string
+	 */
+	function Flags()
+	{
+		$static_name="_STATIC_" . strtoupper(__CLASS__);
+		if (!isset($GLOBALS[$static_name])) $GLOBALS[$static_name]='ims';
+		return $GLOBALS[$static_name];
+	}
+
+	/**
+	 * Set PCRE flags
+	 *
+	 * @access 	public
+	 * @param string $flags Regular expression modifier flags
+	 * @return void
+	 */
+	function SetFlags($flags)
+	{
+		$static_name="_STATIC_" . strtoupper(__CLASS__);
+		$GLOBALS[$static_name]=$flags;
+	}
+
+	/**
+	 * Match method
+	 *
+	 * IDSRegexpFilter->match() used preg_match() to match the rule against
+	 * the given string.
+	 *
+	 * @access 	public
+	 * @return	bool Filter matched?
+	 */
 	function Match($string)
 	{
 		if (!is_string($string)) error('Invalid argument. Exptected a string, got ' . gettype($string));
-		return (bool) preg_match('/' . $this->Rule() . '/iDs', $string);
+		return (bool) preg_match('/' . $this->Rule() . '/' . IDSRegexpFilter::Flags(), $string);
 	}
 }
 ?>
--- Lib/IDSStorageProvider.class.php
+++ Lib/IDSStorageProvider.class.php
@@ -1,25 +1,86 @@
 <?php
+/**
+ * PHP4IDS
+ *
+ * Requirements: PHP4, DOM XML
+ *
+ * Copyright (c) 2007 Stefan Gehrig (gehrig@ishd.de)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @package PHP4IDS
+ * @subpackage Storage
+ */
+
 require_once(dirname(__FILE__) . "/IDSFilter.class.php");
 
+/**
+ * Abstract Filter Storage
+ *
+ * Class to assure the systems API
+ *
+ * @author Stefan Gehrig (gehrig@ishd.de)
+ * @package PHP4IDS
+ * @subpackage Storage
+ * @abstract
+ * @version	0.1
+ */
 class IDSStorageProvider
 {
+	/**
+	* Constructor
+	*
+	* @access	public
+	* @return	void
+	*/
 	function IDSStorageProvider() { }
 
 	/**
-	 * @return array
-	 */
+	* Returns array containing all filters
+	*
+	* @abstract
+	* @access	public
+	* @return	array
+	*/
 	function FilterSet()
 	{
 		error("This method needs to be overridden in subclasses");
 	}
 }
 
+/**
+ * Simple storage class using PHP array
+ *
+ * This class provides filter patterns stored in a PHP array.
+ *
+ * @author Stefan Gehrig (gehrig@ishd.de)
+ * @package PHP4IDS
+ * @subpackage Storage
+ * @version	0.1
+ */
 class IDSSimpleStorageProvider extends IDSStorageProvider
 {
+	/**
+	 * Filterset
+	 *
+	 * @access 	private
+	 * @var 	mixed
+	 */
 	var $mFilterSet=array();
 
 	/**
+	 * Constructor
+	 *
+	 * @access	public
 	 * @param array $filterSet
+	 * @return	void
 	 */
 	function IDSSimpleStorageProvider($filterSet=null)
 	{
@@ -28,6 +89,9 @@ class IDSSimpleStorageProvider extends IDSStorageProvider
 	}
 
 	/**
+	 * Add filters to filter storage array
+	 *
+	 * @access	public
 	 * @param array $filterSet
 	 */
 	function SetFilterSet($filterSet)
@@ -39,11 +103,17 @@ class IDSSimpleStorageProvider extends IDSStorageProvider
 	}
 
 	/**
-	 * @return array
-	 */
+	* Returns array containing all filters
+	*
+	* @access	public
+	* @return	array
+	*/
 	function FilterSet() { return $this->mFilterSet; }
 
 	/**
+	 * Add a single IDSFilter
+	 *
+	 * @access	public
 	 * @param IDSFilter $filter
 	 */
 	function AddFilter(&$filter)
@@ -52,14 +122,37 @@ class IDSSimpleStorageProvider extends IDSStorageProvider
 		$this->mFilterSet[]=&$filter;
 	}
 }
+
+/**
+ * Filter Storage Class
+ *
+ * This class provides filter patterns stored in a xml file
+ * using DOM XML (available only in PHP4)
+ *
+ * @author Stefan Gehrig (gehrig@ishd.de)
+ * @package PHP4IDS
+ * @subpackage Storage
+ * @version	0.1
+ */
 class IDSXmlStorageProvider extends IDSStorageProvider
 {
+	/**
+	 * Either filname to xml filter storage file
+	 * or xml storage file as string.
+	 *
+	 * @access 	private
+	 * @var 	string
+	 */
 	var $mXmlFile=null;
 
 	var $mFilterSet=null;
 
 	/**
+	 * Constructor
+	 *
+	 * @access	public
 	 * @param string $xmlFile
+	 * @return	void
 	 */
 	function IDSXmlStorageProvider($xmlFile)
 	{
@@ -68,14 +161,24 @@ class IDSXmlStorageProvider extends IDSStorageProvider
 	}
 
 	/**
-	 * @return array
-	 */
+	* Returns array containing all filters
+	*
+	* @access	public
+	* @return	array
+	*/
 	function FilterSet()
 	{
 		if (is_null($this->mFilterSet)) $this->mFilterSet=$this->LoadFilterSet();
 		return $this->mFilterSet;
 	}
 
+
+	/**
+	 * Load filters from xml (either file or xml string)
+	 *
+	 * @access private
+	 * @return array
+	 */
 	function LoadFilterSet()
 	{
 		if (is_file($this->mXmlFile))
